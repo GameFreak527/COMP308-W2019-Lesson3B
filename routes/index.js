ff0a44ede850aa60a95a3af46c8bc5c1c75f6f7d
GameFreak527 <shivrana527@gmail.com>
2019-01-26 15:35:30 -0500

    expressJS support

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET products page. */
+router.get('/products', function(req, res, next) {
+  res.render('index', { title: 'products' });
+});
+
+/* GET services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'services' });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'about' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'contact' });
+});
+
 module.exports = router;
